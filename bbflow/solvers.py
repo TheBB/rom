264242ba50b3feb38887752b865ea4d22db4673a
Eivind Fonn <evfonn@gmail.com>
2017-07-28 09:41:35 +0200

    Navier-stokes solver

--- bbflow/solvers.py
+++ bbflow/solvers.py
@@ -5,7 +5,7 @@ from nutils import function as fn, log, plot, _
 import time
 
 
-__all__ = ['stokes']
+__all__ = ['stokes', 'navierstokes']
 
 
 def _time(func):
@@ -24,6 +24,45 @@ def _stokes(case, mu, **kwargs):
     matrix = case.integrate('divergence', mu) + case.integrate('laplacian', mu)
     rhs = case.integrate('lift-divergence', mu) + case.integrate('lift-laplacian', mu)
     lhs = matrix.solve(-rhs, constrain=case.constraints)
+
+    return lhs
+
+
+@_time
+def _navierstokes(case, mu, newton_tol=1e-6, **kwargs):
+    domain = case.domain
+    geom = case.phys_geom(mu)
+
+    stokes_mat = case.integrate('divergence', mu) + case.integrate('laplacian', mu)
+    stokes_rhs = case.integrate('lift-divergence', mu) + case.integrate('lift-laplacian', mu)
+    lhs = stokes_mat.solve(-stokes_rhs, constrain=case.constraints)
+
+    vmass = case.mass('v')
+
+    def lhs_conv_1(vsol):
+        conv = (case.vbasis[:,_,:] * vsol.grad(geom)[_,:,:]).sum(-1)
+        return domain.integrate(fn.outer(case.vbasis, conv).sum(-1), geometry=geom, ischeme='gauss9')
+
+    def lhs_conv_2(vsol):
+        conv = (vsol[_,_,:] * case.vbasis.grad(geom)).sum(-1)
+        return domain.integrate(fn.outer(case.vbasis, conv).sum(-1), geometry=geom, ischeme='gauss9')
+
+    def rhs_conv(vsol):
+        conv = (vsol[_,:] * vsol.grad(geom)).sum(-1)
+        return domain.integrate((case.vbasis * conv[_,:]).sum(-1), geometry=geom, ischeme='gauss9')
+
+    while True:
+        vsol = case.solution(lhs, 'v')
+        rhs = - stokes_rhs - stokes_mat.matvec(lhs) - rhs_conv(vsol)
+        ns_mat = stokes_mat + lhs_conv_1(vsol) + lhs_conv_2(vsol)
+
+        update = ns_mat.solve(rhs, constrain=case.constraints)
+        lhs += update
+
+        update_norm = np.sqrt(vmass.dot(update).dot(update))
+        log.info('Update: {:.2e}'.format(update_norm))
+        if update_norm < newton_tol:
+            break
 
     return lhs
 
@@ -60,3 +99,8 @@ def plots(case, lhs, mu, plot_name='solution', index=0, colorbar=False,
 @log.title
 def stokes(case, **kwargs):
     return _stokes(case, **kwargs)
+
+
+@log.title
+def navierstokes(case, **kwargs):
+    return _navierstokes(case, **kwargs)
