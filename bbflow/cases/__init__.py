12b0a21555dd0a3ebcdc403dbf4d79e8d14534f7
Eivind Fonn <evfonn@gmail.com>
2017-09-25 12:39:33 +0200

    Add airfoil-AoA case

--- bbflow/cases/__init__.py
+++ bbflow/cases/__init__.py
@@ -1,4 +1,6 @@
 from bbflow.cases.bases import ProjectedCase
+
+from bbflow.cases.airfoil import airfoil
 from bbflow.cases.backstep import backstep, backstep_geom, backstep_inlet, backstep_test, mu
 from bbflow.cases.cavity import cavity
 from bbflow.cases.channel import channel
