74b383abcd78c8e4d95bdc0183084704ab2ab877
Eivind Fonn <evfonn@gmail.com>
2017-10-12 12:25:18 +0200

    Inherit from Case class

--- bbflow/cases/cavity.py
+++ bbflow/cases/cavity.py
@@ -1,4 +1,3 @@
-from functools import partial
 import numpy as np
 import scipy as sp
 from nutils import mesh, function as fn, log, _, plot
@@ -6,68 +5,66 @@ from nutils import mesh, function as fn, log, _, plot
 from bbflow.cases.bases import mu, Case
 
 
-def ignore(retval, *args, **kwargs):
-    return retval
+class cavity(Case):
 
+    def __init__(self, refine=1, degree=4, nel=None, **kwargs):
+        if nel is None:
+            nel = int(10 * refine)
 
-def cavity(refine=1, degree=4, nel=None, **kwargs):
-    if nel is None:
-        nel = int(10 * refine)
+        pts = np.linspace(0, 1, nel + 1)
+        domain, geom = mesh.rectilinear([pts, pts])
 
-    pts = np.linspace(0, 1, nel + 1)
-    domain, geom = mesh.rectilinear([pts, pts])
+        super().__init__(domain, geom)
 
-    case = Case(domain, geom)
+        bases = [
+            domain.basis('spline', degree=(degree, degree-1)),  # vx
+            domain.basis('spline', degree=(degree-1, degree)),  # vy
+            domain.basis('spline', degree=degree-1),            # pressure
+            [1],                                                # lagrange multiplier
+            [0] * 4,                                            # stabilization terms
+        ]
+        basis_lens = [len(b) for b in bases]
+        vxbasis, vybasis, pbasis, lbasis, __ = fn.chain(bases)
+        vbasis = vxbasis[:,_] * (1,0) + vybasis[:,_] * (0,1)
 
-    bases = [
-        domain.basis('spline', degree=(degree, degree-1)),  # vx
-        domain.basis('spline', degree=(degree-1, degree)),  # vy
-        domain.basis('spline', degree=degree-1),            # pressure
-        [1],                                                # lagrange multiplier
-        [0] * 4,                                            # stabilization terms
-    ]
-    basis_lens = [len(b) for b in bases]
-    vxbasis, vybasis, pbasis, lbasis, __ = fn.chain(bases)
-    vbasis = vxbasis[:,_] * (1,0) + vybasis[:,_] * (0,1)
+        self.add_basis('v', vbasis, sum(basis_lens[:2]))
+        self.add_basis('p', pbasis, basis_lens[2])
 
-    case.add_basis('v', vbasis, sum(basis_lens[:2]))
-    case.add_basis('p', pbasis, basis_lens[2])
+        self.constrain('v', 'left', 'top', 'bottom', 'right')
 
-    case.constrain('v', 'left', 'top', 'bottom', 'right')
+        self.add_lift(np.zeros(vbasis.shape[0],))
 
-    case.add_lift(np.zeros(vbasis.shape[0],))
+        x, y = geom
+        f = 4 * (x - x**2)**2
+        g = 4 * (y - y**2)**2
+        d1f = f.grad(geom)[0]
+        d1g = g.grad(geom)[1]
+        velocity = fn.asarray((f*d1g, -d1f*g))
+        pressure = d1f * d1g
+        pressure -= domain.integrate(pressure, ischeme='gauss9', geometry=geom) / domain.volume(geometry=geom)
+        force = pressure.grad(geom) - velocity.laplace(geom)
 
-    x, y = geom
-    f = 4 * (x - x**2)**2
-    g = 4 * (y - y**2)**2
-    d1f = f.grad(geom)[0]
-    d1g = g.grad(geom)[1]
-    velocity = fn.asarray((f*d1g, -d1f*g))
-    pressure = d1f * d1g
-    pressure -= domain.integrate(pressure, ischeme='gauss9', geometry=geom) / domain.volume(geometry=geom)
-    force = pressure.grad(geom) - velocity.laplace(geom)
+        self._exact_solutions = {'v': velocity, 'p': pressure}
 
-    case.set_exact('v', partial(ignore, velocity))
-    case.set_exact('p', partial(ignore, pressure))
+        self.add_integrand('forcing', (vbasis * force[_,:]).sum(-1))
+        self.add_integrand('divergence', -fn.outer(vbasis.div(geom), pbasis), symmetric=True)
+        self.add_integrand('laplacian', fn.outer(vbasis.grad(geom)).sum((-1, -2)))
+        self.add_integrand('vmass', fn.outer(vbasis).sum(-1))
+        self.add_integrand('pmass', fn.outer(pbasis))
 
-    case.add_integrand('forcing', (vbasis * force[_,:]).sum(-1))
-    case.add_integrand('divergence', -fn.outer(vbasis.div(geom), pbasis), symmetric=True)
-    case.add_integrand('laplacian', fn.outer(vbasis.grad(geom)).sum((-1, -2)))
-    case.add_integrand('vmass', fn.outer(vbasis).sum(-1))
-    case.add_integrand('pmass', fn.outer(pbasis))
+        self.add_integrand('stab-lhs', fn.outer(lbasis, pbasis), symmetric=True)
 
-    case.add_integrand('stab-lhs', fn.outer(lbasis, pbasis), symmetric=True)
+        points = [
+            (0, (0, 0)),
+            (nel-1, (0, 1)),
+            (nel*(nel-1), (1, 0)),
+            (nel**2-1, (1, 1)),
+        ]
+        eqn = (pbasis.grad(geom) - vbasis.laplace(geom))[:,0,_]
+        self.add_collocate('stab-lhs', eqn, points, index=self.root+1, symmetric=True)
+        self.add_collocate('stab-rhs', force[0,_], points, index=self.root+1)
 
-    points = [
-        (0, (0, 0)),
-        (nel-1, (0, 1)),
-        (nel*(nel-1), (1, 0)),
-        (nel**2-1, (1, 1)),
-    ]
-    eqn = (pbasis.grad(geom) - vbasis.laplace(geom))[:,0,_]
-    case.add_collocate('stab-lhs', eqn, points, index=case.root+1, symmetric=True)
-    case.add_collocate('stab-rhs', force[0,_], points, index=case.root+1)
+        self.finalize()
 
-    case.finalize()
-
-    return case
+    def _exact(self, mu, field):
+        return self._exact_solutions[field]
