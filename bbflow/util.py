e699aa0aaf1e8994fb62502debba9c12f9e453df
Eivind Fonn <evfonn@gmail.com>
2017-10-12 14:59:20 +0200

    Forgotten file :-s

---
+++ bbflow/util.py
@@ -0,0 +1,26 @@
+import inspect
+import functools
+
+
+def multiple_to_single(argname):
+    def decorator(func):
+        signature = inspect.signature(func)
+
+        @functools.wraps(func)
+        def ret(*args, **kwargs):
+            binding = signature.bind(*args, **kwargs)
+            args = binding.arguments[argname]
+            multiple = True
+            if not isinstance(args, (list, tuple)):
+                multiple = False
+                args = [args]
+            retvals = []
+            for arg in args:
+                binding.arguments[argname] = arg
+                retvals.append(func(*binding.args, **binding.kwargs))
+            if not multiple:
+                return retvals[0]
+            return retvals
+
+        return ret
+    return decorator
