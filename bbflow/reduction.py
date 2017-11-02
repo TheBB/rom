c5c68a90528576b662ffa0a8218ce1cc0fa908c5
Eivind Fonn <evfonn@gmail.com>
2017-11-02 14:18:57 +0100

    Add supremizer stabilization

--- bbflow/reduction.py
+++ bbflow/reduction.py
@@ -34,67 +34,13 @@ def plot_spectrum(decomp, show=False, figsize=(10,10), plot_name='spectrum', ind
             plt.show()
 
 
-def nmodes(decomp, max_out=50, **kwargs):
-    all_eigvals = []
-    for fieldid, (field, (evs, __)) in enumerate(decomp.items()):
-        all_eigvals.extend(zip(evs / sum(evs), repeat(fieldid)))
-    all_eigvals = sorted(all_eigvals, key=itemgetter(0), reverse=True)
-
-    num_modes = [0] * len(decomp)
-    added = [False] * len(decomp)
-    errs = [1.0] * len(decomp)
-
-    i = 0
-    for ev, fieldid in all_eigvals:
-        num_modes = list(num_modes)
-        num_modes[fieldid] += 1
-        errs = list(errs)
-        errs[fieldid] = sum(v for v, fid in all_eigvals[i+1:] if fid == fieldid)
-        added[fieldid] = True
-        if not all(added):
-            continue
-        if num_modes[1] >= num_modes[0]:
-            continue
-        added = [False] * len(decomp)
-        yield num_modes, [np.sqrt(max(err, 0)) for err in errs]
-        i += 1
-        if i == max_out:
-            break
-
-
-def reduce(case, ensemble, decomp, threshold=None, nmodes=None, min_modes=None, **kwargs):
+def reduced_bases(case, ensemble, decomp, nmodes):
     nsnapshots = ensemble.shape[0]
-    if min_modes == -1:
-        min_modes = nsnapshots
-
-    if nmodes is None:
-        assert threshold
-        if isinstance(threshold, (float, int)):
-            threshold = (threshold,) * len(decomp)
-        nmodes = OrderedDict()
-        for error, (field, (evs, __)) in zip(threshold, decomp.items()):
-            limit = (1 - error ** 2) * np.sum(evs)
-            try:
-                num = min(np.where(np.cumsum(evs) > limit)[0]) + 1
-                if min_modes:
-                    num = max(num, min_modes)
-            except ValueError:
-                num = nsnapshots
-            if num == nsnapshots and min_modes != nsnapshots:
-                log.warning('All DoFs used, ensemble is probably too small')
-            actual_error = np.sqrt(max(0.0, np.sum(evs[num:]) / np.sum(evs)))
-            log.user('{}: {} modes suffice for {:.2e} error (threshold {:.2e})'.format(
-                field, num, actual_error, error,
-            ))
-            nmodes[field] = num
-        if 'v' in nmodes and 'p' in nmodes:
-            nmodes['v'] = max(nmodes['v'], nmodes['p'] + 1)
-        nmodes = list(nmodes.values())
 
     if isinstance(nmodes, int):
         nmodes = (nmodes,) * len(decomp)
 
-    projection, lengths = [], []
+    bases = OrderedDict()
     for num, (field, (evs, eigvecs)) in zip(nmodes, decomp.items()):
         reduced = ensemble.T.dot(eigvecs[:,:num]) / np.sqrt(evs[:num])
         indices = case.basis_indices(field)
@@ -102,14 +48,34 @@ def reduce(case, ensemble, decomp, threshold=None, nmodes=None, min_modes=None, 
         mask[indices] = 0
         reduced[mask,:] = 0
 
-        projection.append(reduced)
-        lengths.append(num)
+        bases[field] = reduced.T
 
-    return np.concatenate(projection, axis=1).T, lengths
+    return bases
 
 
-def make_reduced(case, ensemble, decomp, **kwargs):
-    projection, lengths = reduce(case, ensemble, decomp, **kwargs)
+def infsup(case, quadrule):
+    mu = case.parameter()
+    vind, pind = case.basis_indices(['v', 'p'])
+
+    # Ensure that Vmass = Pmass = I
+    for name, ind in [('vmass', vind), ('pmass', pind)]:
+        mass = case[name](mu, wrap=False)[np.ix_(ind,ind)]
+        np.testing.assert_almost_equal(np.diag(mass), 1.0)
+        np.testing.assert_almost_equal(mass - np.diag([1.0] * mass.shape[0]), 0.0)
+
+    bound = np.inf
+    for mu, __ in quadrule:
+        mu = case.parameter(*mu)
+        mx = case['divergence'](mu, wrap=False)[np.ix_(pind,vind)]
+        mx = mx.dot(mx.T)
+        ev = np.sqrt(np.linalg.eigvalsh(mx)[0])
+        bound = min(bound, ev)
+
+    return bound
+
+
+def make_reduced(case, ensemble, decomp, nmodes):
+    projection, lengths = reduce(case, ensemble, decomp, nmodes)
     projcase = ProjectedCase(case, projection, lengths, fields=list(decomp))
 
     projcase.meta['nmodes'] = dict(zip(decomp, lengths))
@@ -135,24 +101,3 @@ def make_reduced_parallel(case, ensemble, decomp, nmodes):
     pool = Pool()
     cases = list(log.iter('case', pool.imap(_make_reduced_parallel, args)))
     return cases
-
-
-def infsup(case, quadrule):
-    mu = case.parameter()
-    vind, pind = case.basis_indices(['v', 'p'])
-
-    # Ensure that Vmass = Pmass = I
-    for name, ind in [('vmass', vind), ('pmass', pind)]:
-        mass = case[name](mu, wrap=False)[np.ix_(ind,ind)]
-        np.testing.assert_almost_equal(np.diag(mass), 1.0)
-        np.testing.assert_almost_equal(mass - np.diag([1.0] * mass.shape[0]), 0.0)
-
-    bound = np.inf
-    for mu, __ in quadrule:
-        mu = case.parameter(*mu)
-        mx = case['divergence'](mu, wrap=False)[np.ix_(pind,vind)]
-        mx = mx.dot(mx.T)
-        ev = np.sqrt(np.linalg.eigvalsh(mx)[0])
-        bound = min(bound, ev)
-
-    return bound
