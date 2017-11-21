0549f3088f2fa6d930501d110a43ddb1ccd9eb12
Eivind Fonn <evfonn@gmail.com>
2017-11-21 18:45:03 +0100

    channel: update

--- bbflow/cases/channel.py
+++ bbflow/cases/channel.py
@@ -2,12 +2,15 @@ import numpy as np
 import scipy as sp
 from nutils import mesh, function as fn, log, _, plot
 
-from bbflow.cases.bases import mu, Case
+from bbflow.cases import mu
+from bbflow.util import collocate
+from bbflow.cases.bases import Case
+from bbflow.newaffine import NutilsDelayedIntegrand
 
 
 class channel(Case):
 
-    def __init__(self, refine=1, degree=3, nel=None):
+    def __init__(self, refine=1, degree=3, nel=None, override=False):
         if nel is None:
             nel = int(10 * refine)
 
@@ -37,19 +40,21 @@ class channel(Case):
 
         self._exact_solutions = {'v': profile, 'p': 4 - 2*x}
 
-        self.add_integrand('divergence', -fn.outer(vbasis.div(geom), pbasis), symmetric=True)
-        self.add_integrand('laplacian', fn.outer(vbasis.grad(geom)).sum((-1, -2)))
-        self.add_integrand('vmass', fn.outer(vbasis).sum(-1))
-        self.add_integrand('pmass', fn.outer(pbasis))
-
-        itg = (vbasis[:,_,_,:,_] * vbasis[_,:,_,_,:] * vbasis.grad(geom)[_,_,:,:,:]).sum([-1, -2])
-        self.add_integrand('convection', itg)
+        self['divergence'] = -fn.add_T(fn.outer(vbasis.div(geom), pbasis))
+        self['laplacian'] = fn.outer(vbasis.grad(geom)).sum((-1, -2))
+        self['vmass'] = fn.outer(vbasis).sum(-1)
+        self['pmass'] = fn.outer(pbasis)
+        self['convection'] = NutilsDelayedIntegrand(
+            'w_ia u_jb v_ka,b', 'ijk', 'wuv',
+            x=geom, w=vbasis, u=vbasis, v=vbasis
+        )
 
         points = [(0, (0,0)), (nel-1, (0,1))]
         eqn = (vbasis.laplace(geom) - pbasis.grad(geom))[:,0,_]
-        self.add_collocate('stab-lhs', eqn, points, symmetric=True)
+        colloc = collocate(domain, eqn, points, self.root, self.size)
+        self['stab-lhs'] = colloc + colloc.T
 
-        self.finalize()
+        self.finalize(override=override, domain=domain, geometry=geom)
 
     def _exact(self, mu, field):
         return self._exact_solutions[field]
