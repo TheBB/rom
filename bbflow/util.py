94e752b1c2b1d650cd713bd58f339fbee9ea5779
Eivind Fonn <evfonn@gmail.com>
2017-11-22 17:41:11 +0100

    util: add assembler classes

--- bbflow/util.py
+++ bbflow/util.py
@@ -2,7 +2,8 @@ import inspect
 import functools
 import time as timemod
 import numpy as np
-import scipy as sp
+import scipy.sparse as sp
+import scipy.sparse._sparsetools as sptools
 from nutils import log
 
 
@@ -66,8 +67,8 @@ def collocate(domain, equation, points, index, size):
     if equation.ndim == 2:
         data = np.transpose(data, (0, 2, 1))
         data = np.reshape(data, (ncomps * len(points), data.shape[-1]))
-        data = sp.sparse.coo_matrix(data)
-        data = sp.sparse.csr_matrix((data.data, (data.row + index, data.col)), shape=(size,)*2)
+        data = sp.coo_matrix(data)
+        data = sp.csr_matrix((data.data, (data.row + index, data.col)), shape=(size,)*2)
     elif equation.ndim == 1:
         data = np.hstack([np.zeros((index,)), data.flatten()])
     else:
@@ -79,3 +80,69 @@ def collocate(domain, equation, points, index, size):
 def characteristic(domain, patches):
     basis = domain.basis_patch()
     return basis.dot([1 if i in patches else 0 for i in range(len(basis))])
+
+
+def assembler(shape, *args):
+    assert len(shape) == len(args)
+    assert len(shape) <= 2
+
+    backend = VectorAssembler if len(shape) == 1 else CSRAssembler
+    return backend(shape, *args)
+
+
+class CSRAssembler:
+
+    def __init__(self, shape, row, col):
+        assert len(shape) == 2
+        assert np.max(row) < shape[0]
+        assert np.max(col) < shape[1]
+
+        order = np.lexsort((row, col))
+        row, col = row[order], col[order]
+        mask = ((row[1:] != row[:-1]) | (col[1:] != col[:-1]))
+        mask = np.append(True, mask)
+        row, col = row[mask], col[mask]
+        inds, = np.nonzero(mask)
+
+        M, N = shape
+        idx_dtype = sp.sputils.get_index_dtype((row, col), maxval=max(len(row), N))
+        self.row = row.astype(idx_dtype, copy=False)
+        self.col = col.astype(idx_dtype, copy=False)
+
+        self.order, self.inds = order, inds
+        self.shape = shape
+        self.idx_dtype = idx_dtype
+
+    def __call__(self, data):
+        data = np.add.reduceat(data[self.order], self.inds)
+
+        M, N = self.shape
+        indptr = np.empty(M+1, dtype=self.idx_dtype)
+        indices = np.empty_like(self.col, dtype=self.idx_dtype)
+        new_data = np.empty_like(data)
+
+        sptools.coo_tocsr(M, N, len(self.row), self.row, self.col, data, indptr, indices, new_data)
+        return sp.csr_matrix((new_data, indices, indptr), shape=self.shape)
+
+
+class VectorAssembler:
+
+    def __init__(self, shape, inds):
+        assert len(shape) == 1
+        assert np.max(inds) < shape[0]
+
+        order = np.lexsort((inds,))
+        inds = inds[order]
+        mask = inds[1:] != inds[:-1]
+        mask = np.append(True, mask)
+        self.row = inds[mask]
+        inds, = np.nonzero(mask)
+
+        self.order, self.inds = order, inds
+        self.shape = shape
+
+    def __call__(self, data):
+        data = np.add.reduceat(data[self.order], self.inds)
+        retval = np.zeros(self.shape, dtype=data.dtype)
+        retval[self.row] = data
+        return retval
