67fe3dfae13e729da87aeb0294e0b59924b41698
Eivind Fonn <evfonn@gmail.com>
2018-03-13 10:46:28 +0100

    Ignore Nutils warning about explicit inflation

--- bbflow/__init__.py
+++ bbflow/__init__.py
@@ -1,5 +1,9 @@
 from nutils import config
+from nutils.warnings import NutilsWarning
 import multiprocessing
+
+import warnings
+warnings.filterwarnings('ignore', message='using explicit inflation; this is usually a bug', category=NutilsWarning)
 
 # core.globalproperties['nprocs'] = multiprocessing.cpu_count()
 # core.globalproperties['log'] = log._mklog()
