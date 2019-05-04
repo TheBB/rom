25d9c78d437f0d13a969b29bb71ead43558e4b2f
Eivind Fonn <evfonn@gmail.com>
2019-05-04 12:23:55 +0200

    Fix some test errors and warnings

--- aroma/reduction.py
+++ aroma/reduction.py
@@ -61,7 +61,7 @@ class Reducer:
     def __call__(self):
         case = self.case
         projections = self.get_projections()
-        total_proj = np.vstack(projections.values())
+        total_proj = np.vstack(list(projections.values()))
 
         rcase = LofiCase(case, total_proj)
 
@@ -101,7 +101,7 @@ class ExplicitReducer(Reducer):
         super().__init__(case)
         self._projections = OrderedDict(kwargs)
         self._bases = OrderedDict([
-            (name, ReducedBasis(name, None, p.shape[0], None))
+            (name, ReducedBasis(name, None, p.shape[0], None, False))
             for name, p in kwargs.items()
         ])
 
